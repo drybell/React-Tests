4194ffc0c7e8ea5c246912094ae1308fb1be40e6
Daniel Ryaboshapka <dan4821@gmail.com>
2020-06-14 13:01:24 -0400

    deleted submodule, reuploaded frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Clock from './components/Clock'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Clock/>   
+    </div>
+  );
+}
+
+export default App;
